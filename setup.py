16d32ebb6753f7cdba70ec4e20908b55486df502
Mike Lundy <mike@fluffypenguin.org>
2011-12-06 14:10:23 -0800

    NO UNDERSCORE

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ else:
     PISTON_VERSION = pistonversion.VERSION
 
 setup(
-    name='swift-keystone2',
+    name=name,
     version=PISTON_VERSION,
     description='Swift Keystone2',
     license='Apache License (2.0)',
